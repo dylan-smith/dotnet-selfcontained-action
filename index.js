06c4683c7b49baaeae3f10805bea75f22502308b
Dylan Smith <dylan-smith@github.com>
2021-09-18 15:44:21 -0500

    trying it with explicitly setting things

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ Object.keys(foo).forEach(function(key) {
   console.log('export ' + key + '="' + foo[key] +'"');
 });
 
+// foo.DYLAN_FOO = "blah";
+// foo.INPUT_INPUT_C = "qwefqfqe";
+
 try {
-    execSync('pwsh run-action.ps1', { env: foo, stdio: 'inherit' });
+    execSync('pwsh run-action.ps1', { env: {...foo, DYLAN_FOO: "blah", "INPUT_INPUT-C": "qwfqwfqwfe"}, stdio: 'inherit' });
 } catch(err) {
     console.log("error!");
     console.log(err.message);
