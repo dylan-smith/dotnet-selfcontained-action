48fa9c10f026cebb67a7d8bc1afe687fdb7b8166
Dylan Smith <dylan-smith@github.com>
2021-09-18 15:07:00 -0500

    printing env variables from JS

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const { execSync } = require('child_process');
 
+var env = process.env;
+
+Object.keys(env).forEach(function(key) {
+  console.log('export ' + key + '="' + env[key] +'"');
+});
+
 execSync('pwsh run-action.ps1', { stdio: 'inherit' });
